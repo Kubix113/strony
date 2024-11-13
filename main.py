bc9962e6de93a219f244681398d84f92d8ba2ffc
Kubix <plenjaub@gmail.com>
2024-11-13 19:07:28 +0100

    saca

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Witaj Świecie!</h1><a href='/test'>Przejdz do storny test</a> "
+
+
+@app.route("/test")
+def test():
+    return "<p>Storna Testowa<p>"
+
+app.run(debug=True)
