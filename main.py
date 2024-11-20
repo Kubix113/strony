5aeb638933eb4ba580682f734c93e15fe92134a0
Kubix <plenjaub@gmail.com>
2024-11-20 19:07:17 +0100

    kalkulator

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
-from flask import Flask
+# Import
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1>Witaj Świecie!</h1><a href='/test'>Przejdz do storny test</a> "
+def result_calculate(size, lights, device):
+# Zmienne umożliwiające obliczenie poboru energii przez urządzenia
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
 
+# Pierwsza strona
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/test")
-def test():
-    return "<p>Storna Testowa</p><a href='/'>Wróc</a>"
+# Druga strona
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
 
+# Trzecia strona
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Obliczenia
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
 app.run(debug=True)
